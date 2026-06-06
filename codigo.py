8fb4322b629bf5017c2cb0f4fcb4563438df2b37
Santiago antonelli <antonelli.san21@gmail.com>
2026-06-06 08:32:00 -0300

    agregado archivos desde git

---
+++ codigo.py
@@ -0,0 +1,8 @@
+ubicacion = "moron"
+distancia_capital = 30
+
+def calcular_distancia_total(distancia_capital):
+    return distancia_capital * 2
+
+distancia_total = calcular_distancia_total(distancia_capital)
+print(f"La distancia total desde mi {ubicacion} a la capital y de regreso es: {distancia_total} km")
